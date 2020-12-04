f07caec6b91c95c0f0f85fba73afc735fab73ca0
agungd3v <agungd3v@gmail.com>
2020-12-04 15:36:41 +0700

    .

--- js/parallaxEffect.js
+++ js/parallaxEffect.js
@@ -2,6 +2,3 @@ window.addEventListener('scroll', () => {
     const distance = window.scrollY
     document.querySelector('.welcome').style.transform = `translateY(${distance * 1 / 2}px)`
 })
-window.addEventListener('wheel', evt => {
-    console.log(evt)
-}, { capture: true, passive: true })
